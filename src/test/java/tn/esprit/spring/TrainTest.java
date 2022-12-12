c9fa4796c8c197345616b95170554683256333b6
arij1605 <arij.ncibi@esprit.tn>
2022-12-12 22:24:53 +0100

    9

---
+++ src/test/java/tn/esprit/spring/TrainTest.java
@@ -0,0 +1,95 @@
+package tn.esprit.spring;
+
+import tn.esprit.spring.entities.Train;
+import tn.esprit.spring.entities.etatTrain;
+import tn.esprit.spring.repository.TrainRepository;
+import tn.esprit.spring.services.ITrainService;
+import tn.esprit.spring.services.TrainServiceImpl;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+
+public class TrainTest  {
+
+
+    @Mock
+    TrainRepository trainRepository;
+
+    @InjectMocks
+    ITrainService trainService = new TrainServiceImpl();
+
+
+
+    Train train = new Train(1,etatTrain.prevu,1);
+    List<Train> listTrains = new ArrayList<Train>() {
+        {
+            add(new Train(1,etatTrain.prevu,1));
+            add(new Train(1,etatTrain.prevu,1));
+        }
+    };
+
+
+   /*  @Test
+    void testRetrieveTrainByid() {
+
+        when(trainRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(train));
+        Train trainq = trainService.retrieveTrain(1L);
+
+        System.out.println(trainq);
+        Assertions.assertNotNull(trainq);
+    }*/
+    @Test
+    void testRetrieveAllProduit() {
+
+        List<Train> trains = new ArrayList();
+        trains.add(new Train());
+        when(trainRepository.findAll()).thenReturn(trains);
+        List<Train> expected = trainService.ListerTrainsIndirects(null, null);
+        Assertions.assertEquals(expected, trains);
+        verify(trainRepository).findAll();
+
+    }
+
+
+
+    @Test
+    void testCreateNewObject() {
+        Train obj = new Train(1,etatTrain.prevu,1);
+
+
+        when(trainRepository.save(isA(Train.class))).thenAnswer(invocation -> (Train) invocation.getArguments()[0]);
+        Train returnedObj = trainService.ajouterTrain();
+        ArgumentCaptor<Train> savedObjectArgument = ArgumentCaptor.forClass(Train.class);
+        verify(trainRepository, times(1)).save(savedObjectArgument.capture());
+        verifyNoMoreInteractions(trainRepository);
+
+        Train savedRestObject = savedObjectArgument.getValue();
+        Assertions.assertNotNull(savedRestObject);
+
+    }
+
+   /*  @Test
+    void testDeleteObject() {
+        Train produite = new Train();
+        produite.setLibelleTrain("new test");
+        produite.setIdTrain(1L);
+        when(trainRepository.findById(produite.getIdTrain())).thenReturn(Optional.of(produite));
+        Train trainq = trainService.retrieveTrain(1L);
+        trainService.deleteTrain(trainq.getIdTrain());
+        verify(trainRepository).deleteById(trainq.getIdTrain());
+    } */
+
+}
---
+++ src/test/java/tn/esprit/spring/VoyageTest.java
@@ -0,0 +1,96 @@
+package tn.esprit.spring;
+
+import tn.esprit.spring.entities.Voyage;
+import tn.esprit.spring.entities.Ville;
+import tn.esprit.spring.repository.VoyageRepository;
+import tn.esprit.spring.services.IVoyageService;
+import tn.esprit.spring.services.VoyageServiceImpl;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+
+public class VoyageTest  {
+
+
+    @Mock
+    VoyageRepository voyageRepository;
+
+    @InjectMocks
+    IVoyageService voyageService = new VoyageServiceImpl();
+
+
+
+    Voyage voyage = new Voyage(1,Ville.tunis);
+    List<Voyage> listVoyages = new ArrayList<Voyage>() {
+        {
+            add(new Voyage(1,Ville.tunis));
+            add(new Voyage(1,Ville.tunis));
+        }
+    };
+
+
+   /*  @Test
+    void testRetrieveVoyageByid() {
+
+        when(voyageRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(voyage));
+        Voyage voyageq = voyageService.retrieveVoyage(1L);
+
+        System.out.println(voyageq);
+        Assertions.assertNotNull(voyageq);
+    }*/
+    @Test
+    void testrecupererAll() {
+
+        List<Voyage> voyages = new ArrayList();
+        voyages.add(new Voyage());
+        when(voyageRepository.findAll()).thenReturn(voyages);
+        List<Voyage> expected = voyageService.recupererAll();
+        Assertions.assertEquals(expected, voyages);
+        verify(voyageRepository).findAll();
+
+    }
+
+
+
+    @Test
+    void testCreateNewObject() {
+        Voyage obj = new Voyage(1,Ville.tunis);
+
+
+        when(voyageRepository.save(isA(Voyage.class))).thenAnswer(invocation -> (Voyage) invocation.getArguments()[0]);
+        Voyage returnedObj = voyageService.ajouterVoyage(obj);
+        ArgumentCaptor<Voyage> savedObjectArgument = ArgumentCaptor.forClass(Voyage.class);
+        verify(voyageRepository, times(1)).save(savedObjectArgument.capture());
+        verifyNoMoreInteractions(voyageRepository);
+
+        Voyage savedRestObject = savedObjectArgument.getValue();
+        Assertions.assertNotNull(savedRestObject);
+
+    }
+    
+
+   /*  @Test
+    void testDeleteObject() {
+        Voyage voyage = new Voyage();
+        voyage.setLibelleVoyage("new test");
+        voyage.setIdVoyage(1L);
+        when(voyageRepository.findById(voyage.getIdVoyage())).thenReturn(Optional.of(voyage));
+        Voyage voyageq = voyageService.retrieveVoyage(1L);
+        voyageService.deleteVoyage(voyageq.getIdVoyage());
+        verify(voyageRepository).deleteById(voyageq.getIdVoyage());
+    } */
+
+}
